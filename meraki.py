a4c4b125a3b41b73e90527fd52390c5bc9daf8ea
dannymccaslin <1402643+dannymccaslin@users.noreply.github.com>
2020-03-05 11:41:06 -0500

    Having trouble with the device lock portion

--- meraki.py
+++ meraki.py
@@ -1,41 +1,60 @@
 #python3
 import requests
 import simplejson
+#import API key from your config file
+from config import merakiAPIKey
 
-merakiAPIKey = "b9767658c5a56827060ec8dda3de347fcf9ab393"
 
+#Set up default headers
 headers = {
     'X-Cisco-Meraki-API-Key': merakiAPIKey,
     'Content-Type': 'application/json',
+    'Accept': '*/*'
 }
+#getOrg function gets the OrgId
+def getOrg():
+    r= requests.get('https://api.meraki.com/api/v0/organizations', headers=headers)
+    c = simplejson.loads(r.text)
+    orgId = c[0]['id']
+    return  orgId
 
-r= requests.get('https://dashboard.meraki.com/api/v0/organizations', headers=headers)
+#getNet function gets the only network ID in our environment
+def getNet():
+    r = requests.get('https://api.meraki.com/api/v0/organizations/'+ getOrg() +'/networks', headers=headers)
+    c = simplejson.loads(r.text)
+    netId = c[0]['id']
+    return netId
 
-c = r.content
-j = simplejson.loads(c)
 
-orgId = j[0]['id']
+#findDevice takes an email address and returns devices IDs that are associated with that address
+def findDevice(email):
+    d = requests.get('https://api.meraki.com/api/v0/networks/'+ getNet() +'/sm/devices?fields=name,id,ownerEmail' , headers=headers)
+    dev = simplejson.loads(d.text)
+    jsList = dev["devices"]
+    idList = []
+    for i in jsList:
+        oemail = i.get('ownerEmail', 'none')
+        if oemail == email:
+            idList.append(i['id'])
+    return idList
+        
 
-n = requests.get('https://dashboard.meraki.com/api/v0/organizations/'+orgId+'/networks', headers=headers)
+#lockDevice takes an email address, which it passes to findDevice. It takes the list that it gets back from findDevice and locks those devices
+def lockDevice(email):
+    lockList = findDevice(email) 
 
-print(n.content)
-c = n.content
-j = simplejson.loads(c)
-netId = j[0]['id']
-print(netId)
+    #This loop should work but it doesn't See https://documenter.getpostman.com/view/7928889/SVmsVg6K?version=latest#5c4e48b6-1a5f-4c60-ae80-26bf1d64ec09
+    for i in lockList:
+        lockstr = "\"{\r\n \"ids\":"
+        lockstr += "\"\\\"" + i + "\\\"\"\r\n}\""
+        print(lockstr)
+    #despite the documentation, this is how the ID needs to be formatted. But I can't figure out how to add a new lock code.   
+    payload = '{"ids":"575334852397107558"}'
+    response = requests.request("PUT", 'https://api.meraki.com/api/v0/networks/'+ getNet() +'/sm/devices/lock' , headers=headers, data= payload)
+    print(response.text.encode('utf8'))
+    
 
-d = requests.get('https://dashboard.meraki.com/api/v0/networks/'+netId+'/sm/devices?fields=name,id,ownerEmail', headers=headers)
 
-dev = simplejson.loads(d.text)
-print(d)
-jsList = dev["devices"]
-dannyiPhone = "575334852397107558"
-#d = requests.get('https://dashboard.meraki.com/api/v0/networks/'+netId+'/sm/' +dannyiPhone+ '/cellularUsageHistory', headers=headers)
-#dev = d.content
-#print(jsList)
-for i in jsList:
-    print(f"Name: {i.get('name')} , ID: {i.get('id')} , Email: {i.get('ownerEmail','none listed')}")
 
- 
-
+lockDevice("danny.mccaslin@frederickwater.com")
 
